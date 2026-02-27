d44a72e4578bc19781d755edee08b65167e3534e
AlbinJonsson <ajonsson883@gmail.com>
2026-02-28 00:52:20 +0100

    Added script for evader to go straight to goal

--- Pretraining_scripts.py
+++ Pretraining_scripts.py
@@ -16,3 +16,21 @@ def chaser_straight_2_evader(state, max_acc: float):
     acc = direction * max_acc
 
     return acc.astype(np.float32)
+
+def evader_straight_2_goal(state, max_acc: float):
+    # Move towards the defender
+
+    evader_pos = state["evader_pos"]
+    goal_pos = state["goal"]
+
+    direction = evader_pos - goal_pos
+    distance = np.linalg.norm(direction)
+
+    if distance < 1e-6:
+        # Same position or extremely close: no clear direction
+        return np.zeros(3, dtype=np.float32)
+    
+    direction = direction / distance  # unit vector
+    acc = direction * max_acc
+
+    return acc.astype(np.float32)
